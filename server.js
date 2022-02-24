39df98fb6c0a8d2a7f6bfd1e16bc2fe3999bdf38
Alec Barney <alecbarney34@gmail.com>
2022-02-24 14:08:39 -0700

    first test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
+})
+
+
+const port = process.env.PORT || 4545
+app.listen(port, () => console.log(`We be runnin on port ${port}!`))
