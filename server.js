4634f733514ca727589393dfebba6a0b5f2f99cc
Alec Barney <alecbarney34@gmail.com>
2022-02-24 15:51:55 -0700

    this should still work

--- server.js
+++ server.js
@@ -11,42 +11,42 @@ var rollbar = new Rollbar({
   // record a generic message and send it to Rollbar
   rollbar.log('Hello world!')
 
-  const app = express()
+const app = express()
 
-  app.use(express.json())
-  app.use('/style', express.static('./public/styles.css'))
-  
-  let students = []
-  
-  app.get('/', (req,res) => {
-      res.sendFile(path.join(__dirname, '/public/index.html'))
-      rollbar.info('html file served successfully.')
-  })
-  
-  app.post('/api/student', (req, res)=>{
-      let {name} = req.body
-      name = name.trim()
-  
-      const index = students.findIndex(studentName=> studentName === name)
-  
-      if(index === -1 && name !== ''){
-          students.push(name)
-          rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
-          res.status(200).send(students)
-      } else if (name === ''){
-          rollbar.error('No name given')
-          res.status(400).send('must provide a name.')
-      } else {
-          rollbar.error('student already exists')
-          res.status(400).send('that student already exists')
-      }
-  
-  })
-  
-  const port = process.env.PORT || 4545
-  
-  // rolly
-  app.use(rollbar.errorHandler())
-  
-  app.listen(port, () => console.log(`Take us to warp ${port}!`))
+app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
+
+let students = []
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
+})
+
+app.post('/api/grocery', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
+const port = process.env.PORT || 4545
+
+// rolly
+app.use(rollbar.errorHandler())
+
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
 
