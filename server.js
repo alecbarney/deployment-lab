3537029134a3cd0722ebfd626b82b9a0c746b81d
Alec Barney <alecbarney34@gmail.com>
2022-02-24 15:01:27 -0700

    johnnys help

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
+//BACK END JS 
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const path = require('path')
+const Rollbar = require("rollbar");
 
-test= []
-
-app.use(express.json())
-app.use('/style', express.static('./public/styles.css'))
-
-
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
 
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
@@ -23,16 +15,69 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-app.post('/api/test', (req, res) =>{
-    let {error} = req.body
-    test.push(error)
-    rollbar.log('this should be an error')
-})
 
-
+app.use(express.json())
 app.use(rollbar.errorHandler())
 
+//GET ENDPOINTS
+app.get(`/`, (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+//ROLLBAR REQS
+app.get('/api/critical', (req,res) => {
+    res.status(200).send(`Critical sent to rollbar`)
+    rollbar.critical('rollbar critical sent')
+})
+
+app.get('/api/error', (req,res) => {
+    res.status(200).send(`error sent to rollbar`)
+    rollbar.error('rollbar error sent')
+})
+
+app.get('/api/info', (req,res) => {
+    res.status(200).send(`info sent to rollbar`)
+    rollbar.info('this should work for info')
+})
+
+app.get('/api/warning', (req,res) => {
+    res.status(200).send(`warning sent to rollbar`)
+    rollbar.warning('this is my warning')
+})
+//sends javascript and css to client--------------------------//
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.js'))
+})
+app.get('/styles.css', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/styles.css'))
+})
+//-----------------------------------------------------------//
 
 
-const port = process.env.PORT || 4545
-app.listen(port, () => console.log(`We be runnin on port ${port}!`))
+//PUT ENDPOINTS
+
+//POST ENDPOINTS
+
+//DELETE ENDPOINTS
+
+let port = process.env.PORT || 3005
+app.listen(port, () => console.log(`Now listening on ${port}`))
+
+app.get('/api/critical', (req,res) => {
+    res.status(200).send(`Critical sent to rollbar`)
+    rollbar.critical(context = 'rollbar critical sent')
+})
+
+app.get('/api/error', (req,res) => {
+    res.status(200).send(`error sent to rollbar`)
+    rollbar.error(context = 'rollbar error sent')
+})
+
+app.get('/api/info', (req,res) => {
+    res.status(200).send(`info sent to rollbar`)
+    rollbar.info(context = 'this should work for info')
+})
+
+app.get('/api/warning', (req,res) => {
+    res.status(200).send(`warning sent to rollbar`)
+    rollbar.warning(context = 'this is my warning')
+}) 
