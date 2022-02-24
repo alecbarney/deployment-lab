09b68d62f4e39dff0e73c4c72da577c6d13edd7d
Alec Barney <alecbarney34@gmail.com>
2022-02-24 14:20:37 -0700

    roll bar added

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'a905973e90044b0bac1c8dddc978d9c3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 
 const port = process.env.PORT || 4545
 app.listen(port, () => console.log(`We be runnin on port ${port}!`))
